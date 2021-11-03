5f5f21b0595740cf4a974ea4a3fee0a9e5b6a675
qtv10291 <quangtuanvu1991@gmail.com>
2021-11-03 12:10:29 +0700

    update connect mongodb

--- utils/connectMongoDB.js
+++ utils/connectMongoDB.js
@@ -3,8 +3,8 @@ import { MongoClient } from 'mongodb'
 export default async function connectMongoDB(databaseName){
   // const user = process.env.NEXT_PUBLIC_MONGODB_USER
   const password = process.env.NEXT_PUBLIC_MONGODB_PASSWORD
-  const url = `mongodb+srv://qtvSub:${password}@cluster0.upom7.mongodb.net/${databaseName}?retryWrites=true&w=majority`
-  // const url = `mongodb+srv://qtvmusicadmin:${password}@cluster0.bjfuk.mongodb.net/${databaseName}?retryWrites=true&w=majority`
+  // const url = `mongodb+srv://qtvSub:${password}@cluster0.upom7.mongodb.net/${databaseName}?retryWrites=true&w=majority`
+  const url = `mongodb+srv://qtvmusicadmin:${password}@cluster0.bjfuk.mongodb.net/${databaseName}?retryWrites=true&w=majority`
   // console.log(url)
   const client = new MongoClient(url)
   await client.connect()
